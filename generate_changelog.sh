434f4352ff4bcdf629abe41cecd25e4963975768
Adam <adam.g@outlook.com>
2019-08-22 17:19:28 +0100

    Updated format

--- generate_changelog.sh
+++ generate_changelog.sh
@@ -2,9 +2,9 @@
 # Generate a new changelog from commits.
 # Copied from https://github.com/helm/charts/pull/16444 might be useful for future.
 
-git log --abbrev-commit --pretty=oneline stable/jenkins/ | while read line ;
+git log --abbrev-commit --pretty=oneline stable/sumologic-fluentd/ | while read line ;
 do
   read -r commit message <<< "$line"
-  version=$(git show $commit:stable/jenkins/Chart.yaml|grep "version:"|sed 's/version: //')
-  printf "| %6s | %s | %s |\n" $version $commit "$message"
+  version=$(git show $commit:stable/sumologic-fluentd/Chart.yaml|grep "version:"|sed 's/version: //')
+  printf "##  %6s\n\n%s\ncommit: %s \n\n" $version "$message" $commit
 done
